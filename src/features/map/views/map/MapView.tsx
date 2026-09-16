b3e94521f2709d5a8946b59a343debe7190f82d5
Radek Pachulski <radoslaw.pachulski@gmail.com>
2026-09-16 22:36:25 +0200

    fix: mount map in MapView via createMap and publish it through MapProvider

--- src/features/map/views/map/MapView.tsx
+++ src/features/map/views/map/MapView.tsx
@@ -1,5 +1,37 @@
-import { MapContainer } from '../../components/map-container/MapContainer'
+import { useEffect, useRef, useState } from 'react'
+import { createMap } from '../../../../application/map/map.init'
+import { useMap } from '../../../../application/map/useMap.hook'
 
 export const MapView = () => {
-  return <MapContainer />
+  const containerRef = useRef<HTMLDivElement>(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const { setMap } = useMap()
+
+  useEffect(() => {
+    const container = containerRef.current
+
+    if (!container) {
+      return
+    }
+
+    const map = createMap(container)
+    map.once('load', () => {
+      setIsLoaded(true)
+    })
+    setMap(map)
+
+    return () => {
+      setMap(null)
+      map.remove()
+    }
+  }, [setMap])
+
+  return (
+    <div
+      ref={containerRef}
+      data-testid="map-container"
+      data-loaded={isLoaded}
+      className="h-full w-full"
+    />
+  )
 }
